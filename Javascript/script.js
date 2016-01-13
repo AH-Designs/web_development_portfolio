bb7d71bc08438d524f4ecc0298d947803bebc287
Amber Maria <AHDesigns@outlook.com>
2016-01-13 23:05:06 +0000

    added Animsition jquery plugin, PAGE CHANGING LINKS EFFECTS, still need to choose my effect

---
+++ Javascript/script.js
@@ -0,0 +1,13 @@
+
+//animsition plugin
+
+    $('.animsition').animsition({
+
+    inClass: 'fade-in-down',
+    outClass: 'fade-out-down',
+    inDuration: 1500,
+    outDuration: 1500,
+
+    linkElement: 'nav a'
+
+    });
